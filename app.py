35c96ab5c464d99f89feec1a2c247b3f949cee50
Victoria Cruz Pintor <victoriacruzpintor@outlook.com>
2023-05-01 16:46:43 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contacto')
+def contacto():
+    return redirect("https://wa.me/+34611150440", code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
